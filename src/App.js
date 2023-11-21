f3bdbb6644136089b007276767e5ef0aa0854cd4
Adarsh Ashok <adarsh_ashok@utexas.edu>
2023-11-21 00:35:02 -0600

    build complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import cool1 from './cool1.jpeg';
+import cool2 from './cool2.jpeg';
+import cool3 from './cool3.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cool1} className="App-logo" alt="logo" />
+        <img src={cool2} className="App-logo" alt="logo" />
+        <img src={cool3} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
